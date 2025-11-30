9fcd85428fc63f46c4695a4ce1b758f8f1b1ebe1
Macari_Stanislav <stasmacari63@gmail.com>
2025-11-30 23:56:29 +0200

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+  output: 'export',
+  distDir: 'out',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
