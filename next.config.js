fcaeb22fcba6b8cbc7369923b2738a1a3059fa51
Macari_Stanislav <stasmacari63@gmail.com>
2025-11-30 23:42:27 +0200

    Update feature

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'adminpanel' // ЗАМЕНИ на название своего репозитория
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 }
 
 module.exports = nextConfig
